88debfcdd9e3430247f8a2c016c72125fd7d9de3
Mehmet <mehmetgokce61@gmail.com>
2023-04-09 22:09:10 +0200

    password

--- 23.04.03 Dom Passwordfield/index.js
+++ 23.04.03 Dom Passwordfield/index.js
@@ -1,10 +1,10 @@
 const pass = document.getElementById("pass");
 const checkbox = document.getElementById("check");
 
-check.addEventListener("click", e => {
+checkbox.addEventListener("click", e => {
     console.log(checkbox.checked);
     if(checkbox.checked === true) {
-        pass.setAttribute("type", "text");
+        pass.setAttribute("type", "text")
     } else {
         pass.setAttribute("type", "password");
     }
